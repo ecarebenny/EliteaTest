17b8a6b897fc90085d2ad76f6245a83c943a4d7b
Benny Jose <35192348+ecarebenny@users.noreply.github.com>
2026-04-13 12:23:57 +0530

    Create tests/epam-client-work.spec.ts

---
+++ tests/epam-client-work.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('EPAM website - Client Work navigation', () => {
+  test('navigate via Services to Client Work', async ({ page }) => {
+    // Test implementation will be added in the next commit
+  });
+});
